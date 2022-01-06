b0b5711e4143a2f0b7fef3cfc124d74d86592322
electroluxv2 <electroluxv2@gmail.com>
2022-01-06 17:47:10 +0100

    Simplelightbox

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,6 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, Inject, PLATFORM_ID, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { RenderedPost } from "../../interfaces/rendered-post.interface";
+// @ts-ignore
+import { SimpleLightbox } from "simplelightbox/src/simple-lightbox.js";
+import { isPlatformBrowser } from "@angular/common";
 
 @Component({
   selector: 'app-blog',
@@ -9,8 +12,15 @@ import { RenderedPost } from "../../interfaces/rendered-post.interface";
   preserveWhitespaces: true,
   encapsulation: ViewEncapsulation.Emulated
 })
-export class BlogComponent {
+export class BlogComponent implements AfterViewInit {
   public post: RenderedPost = this.activatedRoute.snapshot.data['post'];
+  private readonly isBrowser: boolean;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, @Inject(PLATFORM_ID) platformId: string) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  public ngAfterViewInit(): void {
+    this.isBrowser && new SimpleLightbox('.image');
+  }
 }
